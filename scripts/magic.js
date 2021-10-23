985b2818d3adea5534abf317472a025dc1e64d4c
ShahriyarHussain <shossainshihab36@gmail.com>
2021-10-23 14:58:46 +0600

    Add On Click for magic buttons

---
+++ scripts/magic.js
@@ -0,0 +1,28 @@
+const clearit = () => {
+  console.log("clearit");
+  document.getElementById("textArea").value = "";
+};
+
+const capitalize = () => {
+  console.log("capitalize");
+};
+
+const sortAll = () => {
+  console.log("sortAll");
+};
+
+const reverseAll = () => {
+  console.log("reverse");
+};
+
+const stripBlank = () => {
+  console.log("blank");
+};
+
+const addNum = () => {
+  console.log("addnum");
+};
+
+const shuffle = () => {
+  console.log("shuffle");
+};
