d1ccaae2839f0352ae164a77c6bc7899811975b3
ShahriyarHussain <shossainshihab36@gmail.com>
2021-10-23 20:41:59 +0600

    Complete magic

--- scripts/magic.js
+++ scripts/magic.js
@@ -1,28 +1,87 @@
+let toggle = true;
+
 const clearit = () => {
-  console.log("clearit");
-  document.getElementById("textArea").value = "";
+    document.getElementById("textArea").value = "";
 };
 
-const capitalize = () => {
-  console.log("capitalize");
+const toggleCase = () => {
+    if (toggle) {
+        let x = document.getElementById("textArea").value.trim().toLowerCase();
+        document.getElementById("textArea").value = x;
+        toggle = !toggle;
+    } else {
+        let x = document.getElementById("textArea").value.trim().toUpperCase();
+        document.getElementById("textArea").value = x;
+        toggle = !toggle;
+    }
 };
 
 const sortAll = () => {
-  console.log("sortAll");
+    let array = [];
+    document
+        .getElementById("textArea")
+        .value.split("\n")
+        .forEach((element) => {
+            let temp = element.trim();
+            if (temp != "") array.push(element.trim());
+        });
+
+    document.getElementById("textArea").value = array.sort().join("\n");
 };
 
 const reverseAll = () => {
-  console.log("reverse");
+    let array = [];
+    document
+        .getElementById("textArea")
+        .value.split("\n")
+        .forEach((element) => {
+            let temp = element.trim();
+            if (temp != "") array.push(element.trim());
+        });
+
+    document.getElementById("textArea").value = array.reverse().join("\n");
 };
 
 const stripBlank = () => {
-  console.log("blank");
+    let array = [];
+    document
+        .getElementById("textArea")
+        .value.split("\n")
+        .forEach((element) => {
+            let temp = element.trim();
+            if (temp != "") array.push(element.trim());
+        });
+
+    document.getElementById("textArea").value = array.join("\n");
 };
 
 const addNum = () => {
-  console.log("addnum");
+    let array = [];
+    document
+        .getElementById("textArea")
+        .value.split("\n")
+        .forEach((element, index) => {
+            let temp = element.trim();
+            if (temp != "") array.push(index + 1 + ". " + temp);
+        });
+
+    document.getElementById("textArea").value = array.join("\n");
 };
 
 const shuffle = () => {
-  console.log("shuffle");
+    let array = document.getElementById("textArea").value.split("\n");
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    document.getElementById("textArea").value = array.join("\n");
 };
+
+function suffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
